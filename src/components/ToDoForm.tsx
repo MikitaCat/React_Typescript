cebec876030b60d59a52912516df5c1ec1969997
MikitaCat <nikita.proger21@gmail.com>
2021-08-03 23:58:10 +0300

    An input handler has been written with using useState. A handler for pressing the Enter button has been written.

--- src/components/ToDoForm.tsx
+++ src/components/ToDoForm.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const ToDoForm: React.FC = () => {
+  const [title, setTitle] = useState<string>('');
+
+  const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+  };
+
+  const keyPressHandler = (event: React.KeyboardEvent) => {
+    if (event.key === 'Enter') {
+      console.log(title);
+    }
+  };
+
   return (
     <div className="input-field mt2">
-      <input type="text" id="title" placeholder="Task Name..." />
+      <input
+        onChange={changeHandler}
+        onKeyPress={keyPressHandler}
+        value={title}
+        type="text"
+        id="title"
+        placeholder="Task Name..."
+      />
       <label htmlFor="title" className="active">
         Enter The Task Name
       </label>
