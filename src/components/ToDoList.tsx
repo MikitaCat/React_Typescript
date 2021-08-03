c2b7b42139ec94b7b8b36e1378bb6ea25960a1f4
MikitaCat <nikita.proger21@gmail.com>
2021-08-04 01:50:32 +0300

    Styles was updated. App started working :)) But, with some bugs)))

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -9,12 +9,18 @@ const ToDoList: React.FC<ToDoListProps> = ({ todos }) => {
   return (
     <ul>
       {todos.map((todo) => {
+        //Dynamic Styles :))))
+        const classes = ['todo'];
+        if (todo.completed) {
+          classes.push('completed');
+        }
+
         return (
-          <li key={todo.id} className="todo">
+          <li key={todo.id} className={classes.join(' ')}>
             <label>
               <input type="checkbox" checked={todo.completed} />
               <span>{todo.title}</span>
-              <i className="material-icons red-text">Delete</i>
+              <i className="material-icons red-text">delete</i>
             </label>
           </li>
         );
